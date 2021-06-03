4d21a2f42320de209e381d463671df290c7aedd4
Josiah Eakle <josiaheakle@gmail.com>
2021-06-03 13:19:33 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "restaurant-client",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
